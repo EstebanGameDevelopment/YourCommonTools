66b34c5db39cacf013f4cbc38d61fe678c1531db
Esteban <services@yourvrexperience.com>
2018-05-26 22:55:29 +0200

    All the external references to CardboardLoaderVR had been removed to become an independent autonomous class.

--- Utilities/CardboardLoaderVR.cs
+++ Utilities/CardboardLoaderVR.cs
@@ -7,17 +7,31 @@ namespace YourCommonTools
 {
 	public class CardboardLoaderVR : MonoBehaviour
 	{
+		public const string CARDBOARD_ENABLE_COOCKIE = "CARDBOARD_ENABLE_COOCKIE";
 
-		public static void SaveEnableCardboard()
+		// -------------------------------------------
+		/* 
+		* Save a flag to report if we need to use or not the Google VR
+		*/
+		public static void SaveEnableCardboard(bool _enabledCardboard)
 		{
-
+			PlayerPrefs.SetInt(CARDBOARD_ENABLE_COOCKIE, (_enabledCardboard ? 1 : 0));
 		}
 
+		// -------------------------------------------
+		/* 
+		 * Get the if we need to use or not the Google VR
+		 */
 		public static bool LoadEnableCardboard()
 		{
-			return true;
+			return (PlayerPrefs.GetInt(CARDBOARD_ENABLE_COOCKIE, 0) == 1);
 		}
 
+
+		// -------------------------------------------
+		/* 
+		 * Start
+		 */
 		void Start()
 		{
 			if (LoadEnableCardboard())
