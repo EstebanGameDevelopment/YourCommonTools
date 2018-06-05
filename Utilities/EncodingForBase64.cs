4c68efd502d7ed13347f197a1d15eaa46b45e5ea
Esteban <services@yourvrexperience.com>
2018-06-05 20:01:49 +0200

    Cleaned imports.

--- Utilities/EncodingForBase64.cs
+++ Utilities/EncodingForBase64.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace YourCommonTools
+﻿namespace YourCommonTools
 {
 	public static class EncodingForBase64
 	{
