d5bed933c7b61585444e333ecac7349ef27256e5
Esteban Gallardo <services@yourvrexperience.com>
2018-08-13 21:41:21 +0200

    DroneKit controller.

---
+++ DroneKit/Scripts/DroneKitController.cs
@@ -0,0 +1,137 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using Microsoft.Scripting.Hosting;
+#if ENABLE_DRONECONTROLLER
+using IronPython.Hosting;
+#endif
+
+namespace YourCommonTools
+{
+	/******************************************
+	 * 
+	 * Runs a python script that allow to change the velocity vector
+     * of the drone
+	 * 
+	 * @author Esteban Gallardo
+	 */
+	public class DroneKitController : MonoBehaviour 
+	{
+        // ----------------------------------------------
+        // EVENTS
+        // ----------------------------------------------
+        public const string EVENT_DRONEKITCONTROLLER_START_VELOCITY     = "EVENT_DRONEKITCONTROLLER_START_VELOCITY";
+        public const string EVENT_DRONEKITCONTROLLER_FINISHED_VELOCITY  = "EVENT_DRONEKITCONTROLLER_FINISHED_VELOCITY";
+
+        // ----------------------------------------------
+        // CONSTANTS
+        // ----------------------------------------------
+        public const string DRONEKIT_TAG_TOTALTIME = "$TOTALTIME";
+        public const string DRONEKIT_TAG_TOTALSPEED = "$TOTALSPEED";
+        public const string DRONEKIT_TAG_PARAMVX = "$PARAMVX";
+        public const string DRONEKIT_TAG_PARAMVY = "$PARAMVY";
+        public const string DRONEKIT_TAG_PARAMVZ = "$PARAMVZ";
+        public const string DRONEKIT_TAG_RETURNHOME = "$RETURNHOME";
+
+        // ----------------------------------------------
+        // SINGLETON
+        // ----------------------------------------------
+        private static DroneKitController _instance;
+
+        public static DroneKitController Instance
+        {
+            get
+            {
+                if (!_instance)
+                {
+                    _instance = GameObject.FindObjectOfType<DroneKitController>();
+                }
+                return _instance;
+            }
+        }
+
+        // ----------------------------------------------
+        // PUBLIC MEMBERS
+        // ----------------------------------------------
+        public TextAsset ScriptDroneKitVelocity;
+
+        // ----------------------------------------------
+        // PRIVATE MEMBERS
+        // ----------------------------------------------
+#if ENABLE_DRONECONTROLLER
+        private ScriptScope m_scope;
+        private ScriptSource m_source;
+#endif
+        private string m_drokeKitScript;
+        private float m_timeout = -1;
+        private Vector3 m_nextVelocity = Vector3.zero;
+        private bool m_returnHome = false;
+        private int m_totalTime = 2;
+        private int m_totalSpeed = 5;
+
+        // -------------------------------------------
+        /* 
+		 * Start the drone with the desired vector velocity
+		 */
+        public void RunVelocity(float _vx, float _vy, float _vz, bool _returnHome = false, int _totalTime = 2, int _totalSpeed = 5)
+		{
+            if (m_timeout >= 0)
+            {
+                m_nextVelocity = new Vector3(_vx, _vy, _vz);
+                m_returnHome = _returnHome;
+                m_totalTime = _totalTime;
+                m_totalSpeed = _totalSpeed;
+                return;
+            }
+
+#if ENABLE_DRONECONTROLLER
+            var engine = Python.CreateEngine();
+            m_scope = engine.CreateScope();
+
+            m_drokeKitScript = ScriptDroneKitVelocity.text;
+            m_drokeKitScript = m_drokeKitScript.Replace(DRONEKIT_TAG_TOTALTIME, _totalTime.ToString());
+            m_drokeKitScript = m_drokeKitScript.Replace(DRONEKIT_TAG_TOTALSPEED, _totalSpeed.ToString());
+            m_drokeKitScript = m_drokeKitScript.Replace(DRONEKIT_TAG_PARAMVX, _vx.ToString());
+            m_drokeKitScript = m_drokeKitScript.Replace(DRONEKIT_TAG_PARAMVY, _vz.ToString());
+            m_drokeKitScript = m_drokeKitScript.Replace(DRONEKIT_TAG_PARAMVZ, _vy.ToString());
+            m_drokeKitScript = m_drokeKitScript.Replace(DRONEKIT_TAG_RETURNHOME, (_returnHome?"1":"0"));
+
+            m_timeout = 0;
+            m_source = engine.CreateScriptSourceFromString(m_drokeKitScript);
+            m_source.Execute(m_scope);
+
+            BasicSystemEventController.Instance.DispatchBasicSystemEvent(EVENT_DRONEKITCONTROLLER_START_VELOCITY);
+#endif
+        }
+
+        // -------------------------------------------
+        /* 
+		 * Check finished
+		 */
+        private void Update()
+        {
+#if ENABLE_DRONECONTROLLER
+            if (m_timeout >= 0)
+            {
+                m_timeout += Time.deltaTime;
+                if (m_timeout > 0.2f)
+                {
+                    m_timeout = 0;
+                    if (m_scope.GetVariable<bool>("vehicleactivated"))
+                    {
+                        m_timeout = -1;
+                        if ((m_nextVelocity != Vector3.zero) || m_returnHome)
+                        {
+                            RunVelocity(m_nextVelocity.x, m_nextVelocity.y, m_nextVelocity.z, m_returnHome, m_totalTime, m_totalSpeed);
+                        }
+                        else
+                        {
+                            BasicSystemEventController.Instance.DispatchBasicSystemEvent(EVENT_DRONEKITCONTROLLER_FINISHED_VELOCITY);
+                        }                        
+                    }
+                }
+            }
+#endif
+        }
+    }
+}
