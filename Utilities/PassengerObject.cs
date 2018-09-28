28fe957eacdb6221f1b071f9794c320d2cda0fef
Esteban Gallardo <services@yourvrexperience.com>
2018-09-28 23:15:41 +0200

    GameObject that links to another and it keeps alive after the linked object has been destroyed.

---
+++ Utilities/PassengerObject.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace YourCommonTools
+{
+
+    /******************************************
+	 * 
+	 * PassengerObject
+	 * 
+	 * @author Esteban Gallardo
+	 */
+    public class PassengerObject : MonoBehaviour
+	{
+        public GameObject MainObject;
+
+        private bool m_isAlive = true;
+
+        private void Update()
+        {
+            if (MainObject != null)
+            {
+                transform.position = MainObject.transform.position;
+            }
+            else
+            {
+                if (m_isAlive)
+                {
+                    m_isAlive = false;
+                    GameObject.Destroy(this.gameObject, 3);
+                }                
+            }            
+        }
+    }
+}
