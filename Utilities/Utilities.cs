597c3bd305a0fcd6c3badb19cb9a746119e6e622
Esteban Gallardo <services@yourvrexperience.com>
2018-10-02 20:33:24 +0200

    New functionality for raycasting.

--- Utilities/Utilities.cs
+++ Utilities/Utilities.cs
@@ -238,11 +238,40 @@ namespace YourCommonTools
 			return false;
 		}
 
-		// ---------------------------------------------------
-		/**
+        // ---------------------------------------------------
+        /**
+		 @brief We get the whole RaycastHit information of the collision, with the mask to consider
+		 */
+        public static bool GetRaycastHitInfoByRayWithMask(Vector3 _origin, Vector3 _forward, ref RaycastHit _hitCollision, params string[] _masksToConsider)
+        {
+            Vector3 fwd = ClonePoint(_forward);
+            fwd.Normalize();
+
+            int layerMask = 0;
+            if (_masksToConsider != null)
+            {
+                for (int i = 0; i < _masksToConsider.Length; i++)
+                {
+                    layerMask |= (1 << LayerMask.NameToLayer(_masksToConsider[i]));
+                }
+            }
+            bool result = false;
+            if (layerMask == 0)
+            {
+                result = Physics.Raycast(_origin, fwd, out _hitCollision, Mathf.Infinity);
+            }
+            else
+            {
+                result = Physics.Raycast(_origin, fwd, out _hitCollision, Mathf.Infinity, layerMask);
+            }
+            return result;
+        }
+
+        // ---------------------------------------------------
+        /**
 		 @brief We get the collided object through the forward vector
 		 */
-		public static GameObject GetCollidedObjectByRay(Vector3 _origin, Vector3 _forward)
+        public static GameObject GetCollidedObjectByRay(Vector3 _origin, Vector3 _forward)
 		{
 			Vector3 fwd = ClonePoint(_forward);
 			fwd.Normalize();
