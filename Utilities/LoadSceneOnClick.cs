42fd6c57cdcf8edd790493a816279bd3a988a3cf
Esteban Gallardo <services@yourvrexperience.com>
2018-11-27 18:18:39 +0100

    Load a defined scene with just a button mouse click.

---
+++ Utilities/LoadSceneOnClick.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace YourCommonTools
+{
+
+    /******************************************
+	 * 
+	 * LoadSceneOnClick
+	 * 
+	 * @author Esteban Gallardo
+	 */
+    public class LoadSceneOnClick : MonoBehaviour
+	{
+        public string NameScene;
+
+        public void OnGUI()
+        {
+            GUI.Label(new Rect(0, 0, 100, 100), "Load scene[" + NameScene + "] on click");
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene(NameScene);
+            }
+        }
+    }
+}
