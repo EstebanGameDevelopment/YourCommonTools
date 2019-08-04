c8748a9dab287df9e5312600d424fabbf4af226a
EstebanGameDevelopment <services@yourvrexperience.com>
2019-08-04 14:03:41 +0200

    Activate only one GameObject

--- Utilities/ActivateLocalModel.cs
+++ Utilities/ActivateLocalModel.cs
@@ -18,16 +18,12 @@ namespace YourCommonTools
         public string Name;
         public GameObject[] Models;
 
-        void Start()
+        public void ActivateModel(int _index)
         {
             for (int i = 0; i < Models.Length; i++)
             {
                 Models[i].SetActive(false);
             }
-        }
-
-        public void ActivateModel(int _index)
-        {
             Models[_index].SetActive(true);
         }
 	}
