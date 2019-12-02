5f452d19b597f769a0e80efe93908a661f8bdeaf
EstebanGameDevelopment <services@yourvrexperience.com>
2019-12-02 13:29:53 +0100

    UDP connection to communicate with Tello Drone

---
+++ DroneKit/Scripts/DroneUDPReceiveController.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace YourCommonTools
+{
+    public class DroneUDPReceiveController : MonoBehaviour
+    {
+        // ----------------------------------------------
+        // EVENTS
+        // ----------------------------------------------	
+        public const string EVENT_DRONEUDPSOCKET_RECEIVED_MESSAGE = "EVENT_DRONEUDPSOCKET_RECEIVED_MESSAGE";
+
+        // ----------------------------------------------
+        // SINGLETON
+        // ----------------------------------------------	
+        private static DroneUDPReceiveController _instance;
+
+        public static DroneUDPReceiveController Instance
+        {
+            get
+            {
+                if (!_instance)
+                {
+                    _instance = GameObject.FindObjectOfType(typeof(DroneUDPReceiveController)) as DroneUDPReceiveController;
+                    if (!_instance)
+                    {
+                        GameObject container = new GameObject();
+                        DontDestroyOnLoad(container);
+                        container.name = "DroneUDPReceiveController";
+                        _instance = container.AddComponent(typeof(DroneUDPReceiveController)) as DroneUDPReceiveController;
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        // ----------------------------------------------
+        // PRIVATE MEMBERS
+        // ----------------------------------------------	
+        private UdpClient m_client;
+
+        // -------------------------------------------
+        /* 
+		 * Init
+		 */
+        public void Init(int _port)
+        {
+            m_client = new UdpClient(_port);
+        }
+
+        // -------------------------------------------
+        /* 
+		 * ReceiveData
+		 */
+        private void ReceiveData()
+        {
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = m_client.Receive(ref anyIP);
+
+                string text = Encoding.UTF8.GetString(data);
+
+                // Den abgerufenen Text anzeigen.
+                Debug.LogError("DroneUDPReceiveController::ReceiveData::TEXT=" + text);
+
+                BasicSystemEventController.Instance.DispatchBasicSystemEvent(EVENT_DRONEUDPSOCKET_RECEIVED_MESSAGE, text);
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+
+        // -------------------------------------------
+        /* 
+		 * Update
+		 */
+        private void Update()
+        {
+            ReceiveData();
+        }
+    }
+}
---
+++ DroneKit/Scripts/DroneUDPSendController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace YourCommonTools
+{
+
+    public class DroneUDPSendController : MonoBehaviour
+    {
+        // ----------------------------------------------
+        // SINGLETON
+        // ----------------------------------------------	
+        private static DroneUDPSendController _instance;
+
+        public static DroneUDPSendController Instance
+        {
+            get
+            {
+                if (!_instance)
+                {
+                    _instance = GameObject.FindObjectOfType(typeof(DroneUDPSendController)) as DroneUDPSendController;
+                    if (!_instance)
+                    {
+                        GameObject container = new GameObject();
+                        DontDestroyOnLoad(container);
+                        container.name = "DroneUDPSendController";
+                        _instance = container.AddComponent(typeof(DroneUDPSendController)) as DroneUDPSendController;
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        // ----------------------------------------------
+        // PRIVATE MEMBERS
+        // ----------------------------------------------	
+        private IPEndPoint m_remoteEndPoint;
+        private UdpClient m_client;
+
+        // -------------------------------------------
+        /* 
+		 * Init
+		 */
+        public void Init(string _ip, int _port)
+        {
+            m_remoteEndPoint = new IPEndPoint(IPAddress.Parse(_ip), _port);
+            m_client = new UdpClient();
+
+            // status
+            Debug.LogError("DroneUDPSendController::Init::Sending to " + _ip + " : " + _port);
+        }
+
+        // -------------------------------------------
+        /* 
+		 * SendString
+		 */
+        private void SendString(string _message)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(_message);
+                m_client.Send(data, data.Length, m_remoteEndPoint);
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+}
