2f76c8f6539abab4331b72ae9df4dfaf244bc615
EstebanGameDevelopment <services@yourvrexperience.com>
2020-06-09 17:44:22 +0200

    Options to build the asset bundle for different platforms

--- AssetBundle/AssetBundleCreator.cs
+++ AssetBundle/AssetBundleCreator.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 namespace YourCommonTools
 {
-    public class ssetBundleCreator : MonoBehaviour
+    public class AssetBundleCreator : MonoBehaviour
     {
-        [MenuItem("Assets/Build Asset Bundle")]
+        [MenuItem("Assets/Build Asset Bundle Android")]
         static void BuildBundles()
         {
             BuildPipeline.BuildAssetBundles("AssetsBundles", BuildAssetBundleOptions.None, BuildTarget.Android);
+        }
+
+        [MenuItem("Assets/Build Asset Bundle IOS")]
+        static void BuildBundlesIOS()
+        {
+            BuildPipeline.BuildAssetBundles("AssetsBundles", BuildAssetBundleOptions.None, BuildTarget.iOS);
+        }
+
+        [MenuItem("Assets/Build Asset Bundle WebGL")]
+        static void BuildBundlesWebGL()
+        {
+            BuildPipeline.BuildAssetBundles("AssetsBundles", BuildAssetBundleOptions.None, BuildTarget.WebGL);
         }
     }
 }
