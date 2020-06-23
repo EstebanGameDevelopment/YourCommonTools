207b593206313aa38bb5d43dc7bb21ba697821c2
EstebanGameDevelopment <services@yourvrexperience.com>
2020-06-23 22:11:11 +0200

    Improments in Subselector component class

--- Utilities/SubmodelSelector.cs
+++ Utilities/SubmodelSelector.cs
@@ -15,7 +15,33 @@ namespace YourCommonTools
 	 */
     public class SubmodelSelector : MonoBehaviour
 	{
+        public const string EVENT_SUBMODEL_SELECTOR_ON_TRIGGER_ENTER = "EVENT_SUBMODEL_SELECTOR_ON_TRIGGER_ENTER";
+
         public string Name;
         public GameObject[] Submodels;
-	}
+
+        void OnTriggerEnter(Collider _collision)
+        {
+            BasicSystemEventController.Instance.DispatchBasicSystemEvent(EVENT_SUBMODEL_SELECTOR_ON_TRIGGER_ENTER, this.gameObject, _collision.gameObject);
+        }
+
+        public void ActivateSubmodel(int _index)
+        {
+            for (int i = 0; i < Submodels.Length; i++)
+            {
+                if (Submodels[i] != null)
+                {
+                    Submodels[i].SetActive(false);
+                }
+            }
+
+            if ((_index>=0)&&(_index< Submodels.Length))
+            {
+                if (Submodels[_index] != null)
+                {
+                    Submodels[_index].SetActive(true);
+                }
+            }
+        }
+    }
 }
