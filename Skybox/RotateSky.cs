cd805309e5f0d29486dee84ef6242f9c785c1b41
DESKTOP-K1COMOS\esteb <services@yourvrexperience.com>
2021-10-09 23:44:48 +0200

    Component pattern for skybox rotation

---
+++ Skybox/RotateSky.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace YourCommonTools
+{
+    public class RotateSky : MonoBehaviour
+    {
+        public float RotateSpeed = 0.8f;
+
+        void Update()
+        {
+            RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+        }
+    }
+}
